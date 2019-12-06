23ecbb23f00d69f84cc77e7ab8a99cea04c4f495
Senthil Kannan <senthilkannanarmy@gmail.com>
2019-12-06 13:31:14 +0530

    Updates

---
+++ precache-manifest.d9c8427aea3c376a04835c57e53074f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0e1cb2452287cd515c1baa76114e1d4",
+    "url": "/react_todo_app/index.html"
+  },
+  {
+    "revision": "a3ac6f3127c24d558495",
+    "url": "/react_todo_app/static/css/main.4063fc26.chunk.css"
+  },
+  {
+    "revision": "9565f49e615632d354bf",
+    "url": "/react_todo_app/static/js/2.897ec109.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo_app/static/js/2.897ec109.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3ac6f3127c24d558495",
+    "url": "/react_todo_app/static/js/main.a6813ab2.chunk.js"
+  },
+  {
+    "revision": "88710791fdd32189c00a",
+    "url": "/react_todo_app/static/js/runtime-main.52e9408c.js"
+  }
+]);
